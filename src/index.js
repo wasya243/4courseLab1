9b25b195338beda62588d2b0f3aad03cbd097ca9
Vasyl Kharchenko <v.kharchenko@mozidev.com>
2018-09-19 23:15:15 +0300

    Add env variables.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const path = require('path');
 
+require('dotenv').config();
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
